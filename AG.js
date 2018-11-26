6a5da71732c63c963d015854179719a505b4ce16
Davluka <Davluka_D@hotmail.com>
2018-11-25 19:22:15 -0600

    Update AG.js
    modificaciones menores en el algoritmo

--- AG.js
+++ AG.js
@@ -3,27 +3,27 @@
 *	Author: Aguilar Moreira David Uriel
 * 	Email : Davluka_D@hotmail.com
 * 	creation_date: 21/11/2018
-* 	objetive:
-* 		the functions must to create a variable what can contains the important features of our genetic
+* 	objetive: 
+* 		the functions must to create a variable what can contains the important features of our genetic 
 *			algorithms in general
-* 	name:
+* 	name: 	
 *		newGA
-*	parametres:
-*		- the selection rule(function) for the population, it must to work with the complete population,
+*	parametres: 
+*		- the selection rule(function) for the population, it must to work with the complete population, 
 *			must return the selected population.
-* 		- the cross rule(function) for the population, 	it must to work with the complete population and
-*			returns a new population with the childrens of the crossover in place of the fathers(it
+* 		- the cross rule(function) for the population, 	it must to work with the complete population and 
+*			returns a new population with the childrens of the crossover in place of the fathers(it 
 *			depends on the fitness).
-*		- the mutation rule(function) for the population, it must to work with the complete population
+*		- the mutation rule(function) for the population, it must to work with the complete population 
 *			and returns a new population what contains a population mutated.
-*		- the function to initialize the population, it must to work for only one element each time,
-*			must to have a paremeter what will contain the current value, and optionally the index and
+*		- the function to initialize the population, it must to work for only one element each time, 
+*			must to have a paremeter what will contain the current value, and optionally the index and 
 *			the array per se.
 * 		- the function to evaluate the fitness of the population on each iteration, it must to work with
 *			one element each time, and save the value of the fitness on each chromosome as a feature.
 *		- the crossover rate for the algorithm, it must to be a number between 0 and 1
 *		- the mutation rate for the algorithm, it must to be a number between 0 and 1
-*
+*		
 *	return:
 *		a variable that contains all the features.
 *
@@ -33,8 +33,8 @@
 function newGA(_Selection,_Cross,_Mutation,_Init,_Fitness,_cross_rate,_mutation_rate,_population_size,_chromosome_size)
 {
 	var _temp_GA = {};
-	_temp_GA.population={};
-	_temp_GA.new_population={};
+	_temp_GA.population=[];
+	_temp_GA.new_population=[];
 	_temp_GA.SelectionFunction 	= _Selection;
 	_temp_GA.CrossoverFunction 	= _Cross;
 	_temp_GA.Mutation_Function 	= _Mutation;
@@ -55,7 +55,7 @@ function newGA(_Selection,_Cross,_Mutation,_Init,_Fitness,_cross_rate,_mutation_
 *	Email: 	Davluka_D@hotmail.com
 *	creation date: 21/11/2018
 *	objetive:
-*		the function must to calculate the genetic algoritm in terms of an object maded from the function
+*		the function must to calculate the genetic algoritm in terms of an object maded from the function 
 *		newGA
 *	name:
 * 		Evolve
@@ -64,23 +64,21 @@ function newGA(_Selection,_Cross,_Mutation,_Init,_Fitness,_cross_rate,_mutation_
 * 	returns:
 *		the last generation
 *
-////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////	
 //////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
 function Evolve(GenAl,max_generations)
 {
 	GenAl.Init(GenAl.population,GenAl.PopulationSize,GenAl.CromosomeSize);
-	console.log(GenAl.population[1].data[1]);
-	console.log(GenAl.population);
-	Array.from(GenAl.population).forEach(function(chromosome) {
-		console.log(chromosome.data);
-		GenAl.Fitness(chromosome);
-	});
+	for(var index =0; index<GenAl.population.length;index++)
+	{
+		GenAl.Fitness(GenAl.population[index]);
+	}
 	for(var index = 0; index<max_generations;index++)
 	{
 		GenAl.new_population	= GenAl.SelectionFunction(GenAl.population);
-		GenAl.new_population	= GenAl.CrossoverFunction(GenAl.new_population,GenAl._cross_rate);
-		GenAl.population	 	= GenAl.Mutation_Function(GenAl.new_population,GenAl._mutation_rate);
+		GenAl.new_population	= GenAl.CrossoverFunction(GenAl.new_population,GenAl._cross_rate);		
+		GenAl.population	 	= GenAl.Mutation_Function(GenAl.new_population,GenAl._mutation_rate);	
 	}
 	return GenAl.population;
 }
